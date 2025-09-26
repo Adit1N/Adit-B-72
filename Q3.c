4242642b172a2ec0e50b3c32c394f950327337db
Adit1N <aditnautiyal10@gmail.com>
2025-09-26 17:45:07 +0530

    Print the address

---
+++ Q3.c
@@ -0,0 +1,12 @@
+//Write a C Program to print the address
+#include <stdio.h>
+
+int main() {
+
+    printf("Your Name: Adit \n");
+    printf("Street Address : 123 Main Street\n");
+    printf("City, State, ZIP Code : Dehradun, Uttarakhand, 248001\n");
+    printf("Country : India\n");
+
+    return 0;
+}
